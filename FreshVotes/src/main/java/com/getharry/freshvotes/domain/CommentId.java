16642dd4b89d80c0a7ed21c7cf89b46302a52a1d
harry <harry@Harry-Laptop.lan>
2024-04-12 18:38:46 -0500

    Entity objects

---
+++ FreshVotes/src/main/java/com/getharry/freshvotes/domain/CommentId.java
@@ -0,0 +1,31 @@
+package com.getharry.freshvotes.domain;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.ManyToOne;
+
+@Embeddable
+public class CommentId implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	@ManyToOne
+	private User user;
+	@ManyToOne
+	private Feature feature;
+	
+	
+	public User getUser() {
+		return user;
+	}
+	public void setUser(User user) {
+		this.user = user;
+	}
+	public Feature getFeature() {
+		return feature;
+	}
+	public void setFeature(Feature feature) {
+		this.feature = feature;
+	}
+
+}
